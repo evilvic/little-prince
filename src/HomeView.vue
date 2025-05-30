bd088d1852e6405f7e693f7105dea7b84487480c
Víctor Peña Romero <vic.pero@icloud.com>
2025-05-30 15:37:48 -0600

    Improve saving

--- src/HomeView.vue
+++ src/HomeView.vue
@@ -38,7 +38,6 @@ onMounted(async () => {
       <SearchComponent />
     </div>
     <div>
-      <h2>Items</h2>
       <p v-if="store.searchActive && store.searchResults.length === 0">
         No results found.
       </p>
